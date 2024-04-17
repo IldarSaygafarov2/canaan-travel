52efc12b75cd16901ca26c76b4ae09789199dbee
sayildar <sayildar17@gmail.com>
2024-04-17 17:34:20 +0500

    added ip address

--- canaan/urls.py
+++ canaan/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path("i18n/", include("django.conf.urls.i18n")),
 ]
