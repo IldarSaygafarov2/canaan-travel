3798522f83625741454d28275a910592b906ed6d
sayildar <sayildar17@gmail.com>
2024-04-20 03:01:52 +0500

    added email fixes

--- canaan/urls.py
+++ canaan/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path("i18n/", include("django.conf.urls.i18n")),
 ]
